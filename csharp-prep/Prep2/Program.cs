c4759998c56ace25219ba6d789e7729f8f39c860
eu826 <gbeccari@byui.edu>
2025-01-08 13:48:48 -0500

    Made exercise 2 csharp-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int grade1 = int.Parse(grade);
+
+        string letter = "";
+        if (grade1 >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade1 >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade1 >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade1 >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade1 >= 50)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You got a {letter}");
+
+        if (grade1 >= 70)
+        {
+        Console.WriteLine($"You passed!");
+        }
+        else if (grade1 < 70)
+        {
+        Console.WriteLine($"You failed!");
+        }
     }
 }
