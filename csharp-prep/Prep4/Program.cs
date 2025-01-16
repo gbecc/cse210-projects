e56b9e60c1090b083e57e690fd44d1e056ce5c3d
eu826 <gbeccari@byui.edu>
2025-01-16 12:06:04 -0500

    Made exercise 5 csharp-5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    int number = -1; 
+    List<int> numbers = new List<int>();
+    while (number != 0)
+    {
+        if (number != 0)
+        {
+        Console.Write("Please enter a number: ");
+        number = int.Parse(Console.ReadLine());
+        numbers.Add(number);
+        }
+    }
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int largest = numbers.Max();
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Largest Number: {largest}");
     }
 }
