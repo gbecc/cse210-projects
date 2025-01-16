0ae385d8525d0ddb5d604cb74895a8c855d77b8c
eu826 <gbeccari@byui.edu>
2025-01-16 14:19:21 -0500

    Correction- made exercise 5 csharp-5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(squaredNumber, userName);
+    }
+        static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome To the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write($"Please enter a username:");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write($"What is your favorite number?");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+        return squared;
+    }
+
+    static void DisplayResult(int squared, string userName)
+    {
+        Console.WriteLine($"Hello {userName}, your number squared is {squared}!");
     }
 }
