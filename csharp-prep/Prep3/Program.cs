55dcfd08a53c8044b434b480a84c71b37ecd38d3
eu826 <gbeccari@byui.edu>
2025-01-16 11:39:32 -0500

    Made exercise 3 scharp-3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+    while (guess != magicNumber)
+    {
+        Console.Write("What is the magic number? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess > magicNumber)
+        {
+            Console.Write("Lower! ");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.Write("Higher! ");
+        }
+        else 
+        {
+            Console.Write("You guessed it! ");
+        }
+    }
     }
 }
