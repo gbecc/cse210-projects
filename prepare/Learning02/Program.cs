0765bfc0d806ccd25c38239df189efff024d8772
eu826 <gbeccari@byui.edu>
2025-01-20 13:56:09 -0500

    Made activity changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+
+        Job job2 = new Job();
+        
+        job2._company = "Apple";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = "2019";
+        job2._endYear = "2022";
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Gabriel Beccari";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
     }
 }
