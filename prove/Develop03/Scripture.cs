8c122f5a06d1aa421019da3de359bddbbfd9c4f7
eu826 <gbeccari@byui.edu>
2025-02-15 12:17:50 -0500

    Small changes

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -12,7 +12,7 @@ class Scripture
     public Scripture(string text)
     {
         activeScripture = text;
-        words = Regex.Split(text, @"(\s+)").ToList(); // Preserves spaces, splits words into array
+        words = Regex.Split(text, @"(\s+)").ToList(); // Preserves spaces, will splitthe words into array
         random = new Random();
     }
 
@@ -24,14 +24,14 @@ class Scripture
     public void SetActiveScripture(string newText)
     {
         activeScripture = newText;
-        words = Regex.Split(newText, @"(\s+)").ToList();
+        words = Regex.Split(newText, @"(\s+)").ToList(); //could use split(' ') but best explanations I found used regex.split
     }
 
     public void WordSubstitution()
     {
         var wordIndexes = words
             .Select((word, index) => new { Word = word, Index = index })
-            .Where(w => w.Word.All(char.IsLetter))
+            .Where(w => w.Word.All(char.IsLetter)) //selects each word along with its index and extracts only the index values (then converting the indexes to a list)
             .Select(w => w.Index)
             .ToList();
 
