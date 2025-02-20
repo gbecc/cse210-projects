ee1681d51b00d34bb32aaeedc40d18172ae5ad6a
eu826 <gbeccari@byui.edu>
2025-02-20 12:47:52 -0500

    First Version

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment ("John", "Mathmatics");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathassignment1 = new MathAssignment("1", "1,2,3,4,5", "John", "Math");
+        Console.WriteLine(mathassignment1.GetSummary());
+        Console.WriteLine(mathassignment1.GetHomeworkList());
+
+        WritingAssignment writingassignment1 = new WritingAssignment("John", "Writing", "Shakespeare");
+        Console.WriteLine (writingassignment1.GetSummary());
+        Console.WriteLine (writingassignment1.GetWritingInformation());
     }
 }
