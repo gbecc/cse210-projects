c61e0ae0ec67985f5da0c09a70005f895732d1b6
eu826 <gbeccari@byui.edu>
2025-02-25 13:24:11 -0500

    Loading Icon

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.ComponentModel;
+using System.Runtime;
+using System.Web;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
+    {   
+    //Console.WriteLine("For example");
+    //     for (int i = 5; i > 0; i--)
+    //     {
+    //         Console.Write(i);
+    //         Thread.Sleep(1000);
+    //         Console.Write("\b \b");
+    //     }
+        Console.WriteLine("Spinner Example:");
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+
+        foreach (string s in animationStrings)
+        {
+            Console.Write(s);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
     }
 }
