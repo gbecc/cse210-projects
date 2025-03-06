8f509cda6fdfa6f26f267602c9554a21ac2aa322
eu826 <gbeccari@byui.edu>
2025-03-06 14:08:33 -0500

    Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 5);
+       
+
+        Circle circle = new Circle("Red", 5);
+        
+
+        Rectangle rectangle = new Rectangle("Red", 5, 7);
+       
+
+        square.SetColor("Blue");
+        
+        rectangle.SetColor("Orange");
+       
+        circle.SetColor("Gold");
+        
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(circle);
+        shapes.Add(square);
+        shapes.Add(rectangle);
+
+        foreach (Shape shapes1 in shapes)
+        {
+            string color = shapes1.GetColor();
+            double area = shapes1.GetArea();
+            Console.WriteLine($"Shape Color: {color}, Area: {area}");
+        }
     }
 }
